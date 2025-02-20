b8a09189bc8bc57d417be01818ff80204d55fc0d
ddcsoftdev <ddcsoftwaredev@gmail.com>
2025-02-20 16:53:17 +0100

    change root name to quote-api

--- quote-api/quote-api/asgi.py
+++ quote-api/quote-api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quote_api project.
+ASGI config for quote-api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quote_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quote-api.settings')
 
 application = get_asgi_application()
--- quote-api/quote-api/wsgi.py
+++ quote-api/quote-api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quote_api project.
+WSGI config for quote-api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quote_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quote-api.settings')
 
 application = get_wsgi_application()
